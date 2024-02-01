06190e19bfa5f73918d51fba965878431d42ff8a
NocturnalRushers <122140360+NocturnalRushers@users.noreply.github.com>
2024-02-01 22:19:26 +0800

    Update tisac.sh

--- tisac.sh
+++ tisac.sh
@@ -53,18 +53,18 @@ tar xf node-v20.10.0-linux-arm64.tar.xz
 echo "export PATH=\$PATH:/root/node-v20.10.0-linux-arm64/bin" >>$current/etc/profile
 fi
 
-echo "正在安装SillyTavern和clewd"
+echo "正在安装SillyTavern————耐心等待……"
 if [ ! -d "SillyTavern" ]; then
 git clone https://github.com/SillyTavern/SillyTavern
 fi
-
+echo "正在安装clewd————耐心等待……"
 git clone -b test https://github.com/teralomaniac/clewd
 
 echo -e "\033[0;33m本操作仅为破限下载提供方便，所有破限皆为收录，不具有破限所有权\033[0m"
 read -p "回车进行导入"
 git clone https://github.com/NocturnalRushers/promot.git st_promot
 if  [ ! -d "st_promot" ]; then
-    echo -e "\n\033[0;33m hoping：因网络波动预设文件下载失败了，更换网络后再试\n\033[0m"
+    echo -e "\n\033[0;33m 因网络波动预设文件下载失败了，更换网络后再试\n\033[0m"
 else
     cp -r $current/root/st_promot/. $current/root/SillyTavern/public/'OpenAI Settings'/
     echo -e "\033[0;33m破限已成功导入，安装完毕后启动酒馆即可看到\033[0m"
